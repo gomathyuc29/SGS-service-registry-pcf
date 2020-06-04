ca4329d8493f384effd1f507d3f575178e0678ee
gomat <gomat@DESKTOP-R60RSH0>
2020-06-04 12:12:25 +0530

    Initial commit of ProducerService

---
+++ ProducerService/src/test/java/com/example/project/HelloControllerTest.java
@@ -0,0 +1,36 @@
+package com.example.project;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class HelloControllerTest {
+	@Autowired
+	WebApplicationContext context;
+	private MockMvc mockMvc;
+	@Before
+	public void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+	}
+	@Test
+	public void helloTest_Ok() throws Exception {
+		String x ="hello world";
+	 mockMvc.perform(get("/hello")).andDo(print()).andExpect(MockMvcResultMatchers.content().string(x)).andExpect(status().isOk());
+	}
+}
